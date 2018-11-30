10355324d1b6f782dd76f4d79a011c69ec048f22
Only <lifedamara@163.com>
2018-11-30 11:27:47 +0800

    :tada: Intial commit

---
+++ config/multiple.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const pagesPath = path.resolve(__dirname, '..', 'src/pages')
+const config = require('../config')
+
+const dirs = fs.readdirSync(pagesPath)
+
+const createMultipleEntries = function () {
+  const entries = {}
+  dirs.forEach(dir => {
+    entries[dir] = `${pagesPath}/${dir}/main.js`
+  })
+  return entries
+}
+
+const createMultipleHtmlPlugins = function () {
+  const devPlugins = []
+  const proPlugins = []
+  dirs.forEach(dir => {
+    devPlugins.push(new HtmlWebpackPlugin({
+      filename: `${dir}.html`,
+      template: `${pagesPath}/${dir}/index.html`,
+      chunks: [dir],
+      inject: true,
+    }))
+    proPlugins.push(new HtmlWebpackPlugin({
+      filename: `${config.build.indexBase}/${dir}.html`,
+      template: `${pagesPath}/${dir}/index.html`,
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      chunksSortMode: 'dependency'
+    }))
+  })
+  return {
+    devPlugins,
+    proPlugins
+  }
+}
+
+const createMultipleDevServerRewrites = function () {
+  const rewrites = []
+  dirs.forEach(dir => {
+    rewrites.push({
+      from: new RegExp(`${dir}(\/*)`),
+      to: path.posix.join(config.dev.assetsPublicPath, `${dir}.html`)
+    })
+  })
+  rewrites.push({
+    from: /.*/,
+    to: path.posix.join(config.dev.assetsPublicPath, `${dirs[0]}.html`)
+  })
+  return rewrites
+}
+
+module.exports = {
+  entries: createMultipleEntries,
+  htmlPlugins: createMultipleHtmlPlugins,
+  rewrites: createMultipleDevServerRewrites,
+}
---
+++ src/pages/user/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>user</h1>
+    <img src="../../assets/logo.png">
+    <button @click="$router.push('login')">登录</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
